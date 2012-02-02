c822859993f48113a70e255ea3d5ea3ec8e316e7
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2012-02-02 11:58:20 +0200

    initial commit

---
+++ src/Request.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * @author Andrey Kucherenko <andrey@kucherenko.org>
+ * @date 01.02.12
+ *
+ */
+class Request
+{
+    private $_source = null;
+
+    /**
+     * Class constructor
+     * @param array $source
+     */
+    public function __construct($source = null) {
+        if (is_null($source)){
+            $source = $_REQUEST;
+        }
+        $this->_source = $source;
+    }
+
+    /**
+     * Get parameter from request
+     *
+     * @param  string $name
+     * @return mixed
+     */
+    public function get($name) {
+//        return isset($this->_source[$name]) ? $this->_source[$name] : null;
+        if (isset($this->_source[$name])) {
+            return $this->_source[$name];
+        } else {
+            return null;
+        }
+    }
+}
---
+++ tests/RequestTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * TestCase for testing work with request.
+ *
+ * @TODOLIST
+ *
+ * + Make object with information about request
+ * + Can make object with custom request source
+ * + Get parameter from request
+ * + Take null if parameter is not exists
+ *
+ */
+
+require_once '../src/Request.php';
+
+/**
+ * @author Andrey Kucherenko <andrey@kucherenko.org>
+ * @date 01.02.12
+ *
+ */
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test for request object creation
+     *
+     * @covers Request::__construct
+     */
+    public function testNewRequest() {
+        $fakeRequest = array('1'=>'1');
+        $request = new Request($fakeRequest);
+        $this->assertAttributeEquals($fakeRequest, '_source', $request);
+    }
+
+    /**
+     * Test request object creation without source
+     *
+     * @covers Request::__construct
+     */
+    public function testNewRequestWithoutSource() {
+        $request = new Request();
+        $this->assertAttributeEquals($_REQUEST, '_source', $request);
+    }
+
+    /**
+     * Test for getting parameter from request.
+     *
+     * @covers Request::get
+     */
+    public function testGetParameter() {
+        $request = new Request(array('test'=>'parameter'));
+
+        $this->assertEquals('parameter', $request->get('test'));
+    }
+
+    /**
+     * Test for getting not existing parameter from request
+     */
+    public function testNotExistingParameter() {
+        $request = new Request(array());
+        $this->assertNull($request->get('test'));
+    }
+
+}
